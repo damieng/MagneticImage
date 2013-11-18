9b2646b9fb421ece06b4e90eb713ebbfac24fbfb
Damien Guard <damien@envytech.co.uk>
2013-11-17 20:46:13 -0800

    First check-in.

---
+++ Source/MagneticImage.Library/Extensions.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DskTool
+{
+	static class Extensions
+	{
+		public static string ReadText(this Stream input, int length)
+		{
+			var buffer = new byte[length];
+			input.Read(buffer, 0, length);
+			return Encoding.ASCII.GetString(buffer);
+		}
+
+		public static ushort ReadWord(this Stream input)
+		{
+			return (ushort)(input.ReadByte() + (input.ReadByte() * 256));
+		}
+
+		public static void Ignore(this Stream input, int length)
+		{
+			input.Seek(length, SeekOrigin.Current);
+		}
+
+		public static byte[] ReadBlock(this Stream input, int length)
+		{
+			var result = new byte[length];
+			input.Read(result, 0, length);
+			return result;
+		}
+
+		public static TReturn MostCommon<TReturn, T>(this IEnumerable<T> source, Func<T, TReturn> property)
+		{
+			return source
+					.GroupBy(s => property(s))
+					.OrderByDescending(g => g.Count())
+					.Select(g => g.Key)
+					.FirstOrDefault();
+		}
+	}
+}
---
+++ Source/MagneticImage.Library/QuotedPrintable.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Linq;
+using System.Text;
+
+namespace DskTool
+{
+	public static class QuotedPrintable
+	{
+		public static string Encode(byte[] bytes)
+		{
+			return Encode(bytes, 0, bytes.Length);
+		}
+
+		public static string Encode(byte[] bytes, int offset, int length)
+		{
+			var output = new StringBuilder();
+			for (var i = offset; i < length; i++) {
+				var b = bytes[i];
+				if (!ShouldBeEncoded(b))
+					output.Append((char)b);
+				else
+					output.Append("=" + b.ToString("X2"));
+			}
+
+			return output.ToString();
+		}
+
+		private static bool ShouldBeEncoded(byte b)
+		{
+			return b < 33 || b > 126 || b == '=';
+		}
+	}
+
+	public static class AsciiHexSwitch
+	{
+		public static string Encode(byte[] bytes)
+		{
+			return Encode(bytes, 0, bytes.Length);
+		}
+
+		public static string Encode(byte[] bytes, int offset, int length)
+		{
+			var binaryMode = false;
+			var output = new StringBuilder();
+
+			byte? lastBinary = null;
+			int lastCount = 0;
+
+			Action flushBinary = () => {
+				if (binaryMode) {
+					if (lastCount == 2)
+						output.Append(lastBinary.Value.ToString("X2"));
+					if (lastCount > 2)
+						output.Append("x" + lastCount + ";");
+				}
+				lastCount = 1;
+				lastBinary = null;
+			};
+
+			for (var i = offset; i < length; i++) {
+				var b = bytes[i];
+				var newBinaryMode = ShouldBeEncoded(b);
+				if (binaryMode != newBinaryMode) {
+					flushBinary();
+					output.Append("~");
+					binaryMode = newBinaryMode;
+				}
+
+				if (binaryMode) {
+					if (lastBinary == b) {
+						lastCount++;
+					} else {
+						flushBinary();
+						output.Append(b.ToString("X2"));
+						lastBinary = b;
+					}
+				}
+				else
+					output.Append((char)b);
+			}
+
+			flushBinary();
+
+			return output.ToString();
+		}
+
+		private static bool ShouldBeEncoded(byte b)
+		{
+			return b < 32 || b > 125;
+		}
+	}
+}
